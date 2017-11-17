1ae34902afa8a5fa0598c8b6a9dca10a2ee1cda7
Ed <eddyburt4@gmail.com>
2017-11-17 17:05:47 +0000

    occupants insert validation

--- Model/Houses/OccupantsInsert.cs
+++ Model/Houses/OccupantsInsert.cs
@@ -2,11 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HouseMoneyAPI.Model {
     public class OccupantInsert {
-        [StringLength (36, ErrorMessage = "The User Id can't exceed 36 characters.")]
         public string UserId { get; set; }
-
-        [Required (ErrorMessage = "A display name is required.")]
-        [StringLength (100, ErrorMessage = "Your display name can't exceed 100 characters.")]
         public string DisplayName { get; set; }
         public int HouseholdId { get; set; }
     }
--- Validation/OccupantsInsertValidator.cs
+++ Validation/OccupantsInsertValidator.cs
@@ -7,7 +7,11 @@ namespace HouseMoneyAPI.Validation
     {
         public OccupantsInsertValidator() {
             RuleFor(x => x.HouseholdId).NotNull();
+            RuleFor(x => x.HouseholdId).GreaterThan(0);
             RuleFor(x => x.UserId).NotNull();
+            RuleFor(x => x.UserId).MaximumLength(36);
+            RuleFor(x => x.DisplayName).NotNull();
+            RuleFor(x => x.DisplayName).MaximumLength(100);
         }
     }
 }
