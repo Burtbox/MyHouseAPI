bff24799bf2535bcd1eb3831a5418bdbbeb92649
Ed <eddyburt4@gmail.com>
2017-11-20 13:32:17 +0000

    fixing db scripts to build db from scratch

---
+++ Database/04_Food/2_Tables/0005_Days_GetNumberOfPeople.sql
@@ -0,0 +1,15 @@
+IF OBJECT_ID('[Food].[Days_GetNumberOfPeople]') IS NULL
+BEGIN
+	execute dbo.sp_executesql @statement = N'
+	CREATE FUNCTION [Food].[Days_GetNumberOfPeople] (@Date date)
+	RETURNS TINYINT
+	AS
+	BEGIN
+		DECLARE @NumberOfPeople TINYINT
+		SELECT @NumberOfPeople = COUNT(*)
+		FROM [Food].[People]
+		WHERE [Date] = @Date
+		RETURN @NumberOfPeople
+	END'
+END
+GO
