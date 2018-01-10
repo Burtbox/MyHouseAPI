6a3fa7383c84c5890fd6d9c1b77e2234e6343f3a
Ed <eddyburt4@gmail.com>
2018-01-10 22:21:59 +0000

    BOOOM!

--- MyHouse_IntegrationTests/Shared/BaseIntegrationTest.cs
+++ MyHouse_IntegrationTests/Shared/BaseIntegrationTest.cs
@@ -21,7 +21,7 @@ namespace MyHouseIntegrationTests.Shared
         }
         public RestRequest apiCall(string endpoint, Method method)
         {
-            string token = generateToken();
+            string token = cheekySneakyToken();
             RestRequest request = new RestRequest(endpoint, method);
 
             // add HTTP Headers
@@ -61,6 +61,25 @@ namespace MyHouseIntegrationTests.Shared
                 expires: now.AddMinutes(50).DateTime,
                 signingCredentials: signingCredentials);
             return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+        private string cheekySneakyToken()
+        {
+            RestClient client = new RestClient("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyA27cRAIaX6NqiLQ4_AHNB91MlHajiTplA");
+            RestRequest request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(
+                new
+                {
+                    token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI3MGFqeFdtclM2WElVNTNHTDZiajFWY2pDc20xIiwiaWF0IjoxNTE1NjIyNzk4LCJleHAiOjE1MTU2MjYzOTgsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHl0b29sa2l0Lmdvb2dsZWFwaXMuY29tL2dvb2dsZS5pZGVudGl0eS5pZGVudGl0eXRvb2xraXQudjEuSWRlbnRpdHlUb29sa2l0IiwiaXNzIjoiZmlyZWJhc2UtYWRtaW5zZGstc2lub2tAbXlob3VzZS1hMDFjNy5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInN1YiI6ImZpcmViYXNlLWFkbWluc2RrLXNpbm9rQG15aG91c2UtYTAxYzcuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20ifQ.cAUf1QJoS3xSa8DXxsr2EFDc6TMvNG6meZQqBlIl72xOurSnaW8h1amisToVWznhlPrLg8YOgSaRraUGi_4t5XYjaaVIZTCMR7pMBnXXz-6mGwSthK3zMwYdNGDJMyIqYTSkVITv5cmCgMLXcB_D7W9uNX2ixSz8DHgBSwp_UMvvZQDTm8gAxlfaxqoGWhCNuXYSi_X2nF2sTMnOCUdNRSfpiWegv9zAvlz57X0IKbcUHjQ6uzn7HHGlMhsZzMOIzkszYLLlEChUgNpXLRj-RBS-0nJK35Ol8vaheMqgz351HRc-NflmrywWqkoJfGWGqrr_sw5GEuvyNSgB_Z0UOg",
+                    returnSecureToken = true
+                }
+            );
+            var reponse = client.Execute(request);
+
+            string token = JsonConvert.DeserializeObject<FirebaseToken>(reponse.Content).idToken;
+            return token;
+
         }
     }
 
---
+++ MyHouse_IntegrationTests/Shared/FirebaseToken.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace MyHouseIntegrationTests.Shared
+{
+    public class FirebaseToken
+    {
+        public string kind;
+        public string idToken;
+        public string refreshToken;
+        public string expiresIn;
+
+        public string isNewUser;
+    }
+
+}
