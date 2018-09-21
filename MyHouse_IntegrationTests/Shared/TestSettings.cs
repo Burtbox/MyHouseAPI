3ea08578486178e1362aa93633f12d41305e2198
Ed <eddyburt4@gmail.com>
2018-09-21 21:21:29 +0100

    adding gets and sets to test settings class

--- MyHouse_IntegrationTests/Shared/TestSettings.cs
+++ MyHouse_IntegrationTests/Shared/TestSettings.cs
@@ -4,24 +4,24 @@ namespace MyHouseIntegrationTests.Shared
 {
     public class TestSettings
     {
-        public string BaseUrl;
-        public string NodeJsExe;
+        public string BaseUrl { get; set; }
+        public string NodeJsExe { get; set; }
 
-        public string H1UserId;
-        public string H1DisplayName;
-        public int H1OccupantId;
-        public string H1Email;
+        public string H1UserId { get; set; }
+        public string H1DisplayName { get; set; }
+        public int H1OccupantId { get; set; }
+        public string H1Email { get; set; }
 
-        public string H2UserId;
-        public string H2DisplayName;
-        public int H2OccupantId;
-        public string H2Email;
+        public string H2UserId { get; set; }
+        public string H2DisplayName { get; set; }
+        public int H2OccupantId { get; set; }
+        public string H2Email { get; set; }
 
-        public string H3UserId;
-        public string H3DisplayName;
-        public int H3OccupantId;
-        public string H3Email;
+        public string H3UserId { get; set; }
+        public string H3DisplayName { get; set; }
+        public int H3OccupantId { get; set; }
+        public string H3Email { get; set; }
 
-        public string FirebaseAdminConsolePath;
+        public string FirebaseAdminConsolePath { get; set; }
     }
 }
